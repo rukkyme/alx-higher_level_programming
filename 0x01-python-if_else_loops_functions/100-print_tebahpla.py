b27b0ad1f09fbc77b7d820a551c78c360a2a8748
Joy <communicatejoy@yahoo.com>
2022-11-29 07:28:53 -0800

    task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,4 +2,4 @@
 for ascii_num in range(122, 96, -1):
     if ascii_num % 2 == 1:
         ascii_num = ascii_num - 32
-    print("{:c}".format(ascii_num), end=''i)
+    print("{:c}".format(ascii_num), end='')
